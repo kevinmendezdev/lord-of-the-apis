19fc77288e3971210423aab95afad8e49fdf5601
Kevin Mendez <kevinrmendez@gmail.com>
2021-09-19 10:26:46 +0300

    create useHeroList hook decouple logic from presentation

--- src/components/SearchDeck.tsx
+++ src/components/SearchDeck.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import Results from './Results';
 import { Hero } from '../model/Hero';
 import Loader from './Loader';
 import ErrorMessage from './ErrorMessage';
 import SearchBar from './SearchBar';
-import * as Const from '../constants';
 import debounce from 'lodash.debounce';
+import useHeroList from '../useHeroList';
 
 function SearchDeck() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [heroes, setHeroes] = useState<Hero[]>([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [heroes, setHeroes] = useState<Hero[]>([]);
   const [deckId, setDeckId] = useState(20);
-  const [loadingStatus, setloadingStatus] = useState(true);
-  const [error, setError] = useState('');
+  // const [loadingStatus, setloadingStatus] = useState(true);
+  // const [error, setError] = useState('');
+  const [heroList, isLoading, hasData, errorMessage] =
+    useHeroList(deckId);
 
-  useEffect(() => {
-    requestDecks();
-  }, []);
+  // useEffect(() => {
+  //   requestDecks();
+  // }, []);
 
   //debounce for avoiding multiple requests of decks
   // const debounceRequestDeck = useCallback(
@@ -24,54 +26,54 @@ function SearchDeck() {
   //   [setDeckId],
   // );
 
-  async function requestDecks() {
-    console.log('request sent');
-    setIsLoading(true);
-    const res = await fetch(
-      `${Const.API_URL}/api/oauth2/deck/load/${deckId}`,
-    );
+  // async function requestDecks() {
+  //   console.log('request sent');
+  //   setIsLoading(true);
+  //   const res = await fetch(
+  //     `${Const.API_URL}/api/oauth2/deck/load/${deckId}`,
+  //   );
 
-    const json = await res.json();
-    console.log(`json: ${loadingStatus}`);
-    if ('success' in json) {
-      setloadingStatus(false);
-      setError(json.error);
-    } else {
-      setloadingStatus(true);
-      if ('heroes' in json) {
-        console.log('heroes in da house');
-        let heroes = Object.keys(json.heroes);
-        let heroesObjects = heroes.map((hero: string) =>
-          fetchHeroe(hero),
-        );
-        Promise.all(heroesObjects).then((heroes: Hero[]) => {
-          setHeroes(heroes);
-        });
-        console.log('heroes');
-        console.log(heroes);
-        console.log(heroesObjects);
-      }
-    }
+  //   const json = await res.json();
+  //   console.log(`json: ${loadingStatus}`);
+  //   if ('success' in json) {
+  //     setloadingStatus(false);
+  //     setError(json.error);
+  //   } else {
+  //     setloadingStatus(true);
+  //     if ('heroes' in json) {
+  //       console.log('heroes in da house');
+  //       let heroes = Object.keys(json.heroes);
+  //       let heroesObjects = heroes.map((hero: string) =>
+  //         fetchHeroe(hero),
+  //       );
+  //       Promise.all(heroesObjects).then((heroes: Hero[]) => {
+  //         setHeroes(heroes);
+  //       });
+  //       console.log('heroes');
+  //       console.log(heroes);
+  //       console.log(heroesObjects);
+  //     }
+  //   }
 
-    setTimeout(() => {
-      console.log(json);
-      setIsLoading(false);
-    }, 2000);
-  }
+  //   setTimeout(() => {
+  //     console.log(json);
+  //     setIsLoading(false);
+  //   }, 2000);
+  // }
 
-  async function fetchHeroe(heroe: string): Promise<Hero> {
-    const res = await fetch(
-      `https://ringsdb.com/api/public/card/${heroe}`,
-    );
-    const json = await res.json();
-    console.log(json);
-    return json;
-  }
+  // async function fetchHeroe(heroe: string): Promise<Hero> {
+  //   const res = await fetch(
+  //     `https://ringsdb.com/api/public/card/${heroe}`,
+  //   );
+  //   const json = await res.json();
+  //   console.log(json);
+  //   return json;
+  // }
 
   function changeHandler(e: React.FormEvent<HTMLInputElement>) {
     setDeckId(+e.currentTarget.value);
     console.log(e.currentTarget.value);
-    requestDecks();
+    // requestDecks();
     // debounceRequestDeck();
   }
 
@@ -81,10 +83,10 @@ function SearchDeck() {
       <>
         {isLoading ? (
           <Loader />
-        ) : loadingStatus ? (
-          <Results heroes={heroes} />
+        ) : hasData ? (
+          <Results heroes={heroList as Hero[]} />
         ) : (
-          <ErrorMessage error={error} />
+          <ErrorMessage error={errorMessage as string} />
         )}
       </>
     </>
---
+++ src/useHeroList.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import { Hero } from './model/Hero';
+
+import * as Const from './constants';
+
+export default function useHeroList(deckId = 20) {
+  const [heroList, setheroList] = useState<Hero[]>([]);
+  // const [deckId, setDeckId] = useState(20);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [hasData, setHasData] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    requestDecks();
+  }, [deckId]);
+
+  async function requestDecks() {
+    console.log('request sent');
+    setIsLoading(true);
+    const res = await fetch(
+      `${Const.API_URL}/api/oauth2/deck/load/${deckId}`,
+    );
+
+    const json = await res.json();
+    console.log(`json: ${hasData}`);
+    if ('success' in json) {
+      setHasData(false);
+      setErrorMessage(json.error);
+    } else {
+      setHasData(true);
+      console.log('heroes loading');
+      if ('heroes' in json) {
+        console.log('heroList in da house');
+        let heroList = Object.keys(json.heroes);
+        let heroListObjects = heroList.map((hero: string) =>
+          fetchHeroe(hero),
+        );
+        Promise.all(heroListObjects).then((heroList: Hero[]) => {
+          setheroList(heroList);
+        });
+        console.log('heroList');
+        console.log(heroList);
+        console.log(heroListObjects);
+      }
+    }
+
+    setTimeout(() => {
+      console.log(json);
+      setIsLoading(false);
+    }, 2000);
+  }
+
+  async function fetchHeroe(heroe: string): Promise<Hero> {
+    const res = await fetch(
+      `https://ringsdb.com/api/public/card/${heroe}`,
+    );
+    const json = await res.json();
+    console.log(json);
+    return json;
+  }
+  return [heroList, isLoading, hasData, errorMessage];
+}
